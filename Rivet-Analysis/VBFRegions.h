62f299a0eafd0b66684c0011e08d67457fa3e403
tinghuagithub <longyiny827841389@gmail.com>
2022-08-10 23:43:05 -0500

    update analysis

--- Rivet-Analysis/VBFRegions.h
+++ Rivet-Analysis/VBFRegions.h
@@ -1,3 +1,6 @@
+#include "Rivet/Analyses/MC_JetAnalysis.hh"
+#include "Rivet/Math/MathUtils.hh"
+#include "Rivet/Projections/ZFinder.hh"
 #include "Rivet/Analysis.hh"
 #include "Rivet/Projections/FinalState.hh"
 #include "Rivet/Projections/FastJets.hh"
@@ -9,55 +12,100 @@
 
 namespace Rivet {
   
+   class NLOAnalysis : public Analysis {
+   public:
 
-  bool VBFRegion0( Jets jets){
-        const FourMomentum& j1(jets[0].momentum());
-        const FourMomentum& j2(jets[1].momentum());
-          // require first two jets the have at least 30GeV of pT
-        return (j1.pT()/GeV > 25.0 ) && (j2.pT()/GeV > 25.0 );
-  }
-  bool VBFRegion1Tight( Jets jets){
-        const FourMomentum& j1(jets[0].momentum());
-        const FourMomentum& j2(jets[1].momentum());
-	double mjjval = (j1+j2).mass()/GeV;
-          // invariant mass cut
-        return (mjjval > 600.0);
-  }
-
-  bool VBFRegion1Loose( Jets jets){
-        const FourMomentum& j1(jets[0].momentum());
-        const FourMomentum& j2(jets[1].momentum());
-	double mjjval = (j1+j2).mass()/GeV;
-          // invariant mass cut
-        return (mjjval > 200.0);
+     NLOAnalysis(string name="NLOAnalysis")
+     : Analysis(name) {
+     }
+     
+     class NLOHisto1D : public YODA::Histo1D {
+     private:
+  
+  YODA::Histo1D* _tmphist;
+  int _current_event_number;
+  
+  void _syncHists() {
+    for (size_t i=0; i<_tmphist->bins().size(); ++i) {
+      if (_tmphist->bin(i).area()) YODA::Histo1D::fillBin(i, _tmphist->bin(i).area());
+    }
+    if (_tmphist->overflow().sumW())  YODA::Histo1D::overflow()+=_tmphist->overflow();
+    if (_tmphist->underflow().sumW()) YODA::Histo1D::underflow()+=_tmphist->underflow();
+    _tmphist->reset();
   }
   
-
-  bool VBFRegion2Tight( Jets jets ){
-        const FourMomentum& j1(jets[0].momentum());
-        const FourMomentum& j2(jets[1].momentum());
-	double dyjjval= fabs(j1.rapidity()-j2.rapidity());
-	// rapidity gap
-	return (dyjjval>4.5);
-  }
-
-  bool VBFRegion2Loose( Jets jets ){
-        const FourMomentum& j1(jets[0].momentum());
-        const FourMomentum& j2(jets[1].momentum());
-	double dyjjval= fabs(j1.rapidity()-j2.rapidity());
-	// rapidity gap
-	return (dyjjval>1.0);
-  }
-
-
-  bool VBFRegion3( Jets jets){
-	const FourMomentum& j1(jets[0].momentum());
-        const FourMomentum& j2(jets[1].momentum());
-	double yopp = j1.rapidity()*j2.rapidity();
-	// opposite detectors 
-	return (yopp < 0.0);
-  }
-
   
+public:
+  
+  NLOHisto1D(size_t nbins, double lower, double upper, const string& path) :
+  YODA::Histo1D(nbins, lower, upper, path),
+  _current_event_number(-1)
+  {
+    _tmphist = new Histo1D(nbins, lower, upper, path+"_tmp");
+  }
+  
+  NLOHisto1D(const vector<double>& binedges, const string& path) :
+  YODA::Histo1D(binedges, path),
+  _current_event_number(-1)
+  {
+    _tmphist = new Histo1D(binedges, path+"_tmp");
+  }
+  
+  ~NLOHisto1D()
+  {
+    delete _tmphist;
+  }
+  
+  void fill(double x, const Event& event)
+  {
+    if (_current_event_number==-1)
+      _current_event_number = event.genEvent()->event_number();
+    
+    if (event.genEvent()->event_number()!=_current_event_number) {
+      _syncHists();
+      _current_event_number = event.genEvent()->event_number();
+    }
+    
+    _tmphist->fill(x, event.weight());
+  }
+  
+  void fillBin(size_t i, const Event& event, const double& fac)
+  {
+    if (_current_event_number==-1)
+      _current_event_number = event.genEvent()->event_number();
+    
+    if (event.genEvent()->event_number()!=_current_event_number) {
+      _syncHists();
+      _current_event_number = event.genEvent()->event_number();
+    }
+    
+    _tmphist->fillBin(i, event.weight()*fac);
+  }
+  
+  void finalize()
+  {
+    _syncHists();
+  }
+  
+};
 
+typedef shared_ptr<NLOHisto1D> NLOHisto1DPtr;
+
+
+NLOHisto1DPtr bookNLOHisto1D(const string& hname,
+                             size_t nbins, double lower, double upper)
+{
+  NLOHisto1DPtr hist(new NLOHisto1D(nbins, lower, upper, histoPath(hname)));
+  addAnalysisObject(hist);
+  return hist;
+}
+
+NLOHisto1DPtr bookNLOHisto1D(const string& hname,
+                             const vector<double>& binedges)
+{
+  NLOHisto1DPtr hist(new NLOHisto1D(binedges, histoPath(hname)));
+  addAnalysisObject(hist);
+  return hist;
+}
+   };
 }
